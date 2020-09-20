8a2a8b24f88e95984fde7f089eed22cb32aed6b7
Dinesh Kumar R <dinesh@brigita.co>
2020-09-20 23:02:01 +0530

    removed unused import

--- src/app/movie/movie.service.ts
+++ src/app/movie/movie.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Subject, throwError } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment';
 import { Movie } from './movie.model';
